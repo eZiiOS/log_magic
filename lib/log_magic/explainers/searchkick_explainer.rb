b79a1b483261effbf8cc3d5c3e720a92f8d673ef
Manuel Arno Korfmann <manu@korfmann.info>
2016-12-27 22:40:26 +0100

    linking to log url works

--- lib/log_magic/explainers/searchkick_explainer.rb
+++ lib/log_magic/explainers/searchkick_explainer.rb
@@ -1,19 +1,40 @@
 require 'rack'
+require 'byebug'
+require 'haml'
 
 class LogMagic::SearchkickExplainer
+  include ::LogMagic::JSONUtils
+
   attr_accessor :database_name
 
-  def initialize
+  def initialize()
     @persistance_layer = ::LogMagic::PersistanceLayer.new
+  end
+
+  def query_json
+    @persistance_layer.retrieve_value(@uuid)
   end
 
   def start
     app = Proc.new do |env|
-      uuid = env['PATH_INFO'][1..-1]
-      body = @persistance_layer.retrieve_value(uuid)
+      @uuid = env['PATH_INFO'][1..-1]
+      body = rendered_template
+
       ['200', {'Content-Type' => 'text/html', 'Content-Length' => body.length.to_s}, [body]]
     end
 
-    Rack::Handler::WEBrick.run app, Port: 7467
+    Rack::Handler::WEBrick.run app, Port: 7467,
+                                    Logger: WEBrick::Log::new('/dev/null')
+  end
+
+  def rendered_template
+    engine = Haml::Engine.new(File.read(template_path))
+    engine.render(self)
+  end
+
+  def template_path
+    File.expand_path(
+      File.join(__FILE__, '..', '..', 'explainer_templates', 'searchkick.haml')
+    )
   end
 end
--- lib/log_magic/utils/json_utils.rb
+++ lib/log_magic/utils/json_utils.rb
@@ -1,6 +1,8 @@
 module LogMagic::JSONUtils
   def pretty_print_json(json)
     JSON.pretty_generate(parse_json(json))
+    .gsub("\n", '<br>')
+    .gsub(" ", '&nbsp;')
   end
 
   def parse_json(json)
